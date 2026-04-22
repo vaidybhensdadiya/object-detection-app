dfc1c8bf192af86802d2f4b0e853f66c3ee8f682
vaidybhensdadiya <bhensdadiyavaidy@gmail.com>
2026-04-22 21:12:31 +0530

    Refactor app.py for object detection functionality

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
-# 🚨 FORCE FIX (VERY IMPORTANT)
-import subprocess
-subprocess.call("pip uninstall -y opencv-python", shell=True)
-
 import cv2
 cv2.setNumThreads(0)
 
